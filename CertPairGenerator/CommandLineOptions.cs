02d5a1f72dc7bcf74461c36464133dbb39c92921
Michael Lynch <mlynch@isecpartners.com>
2013-05-15 19:05:09 -0400

    Removing unused variable from CommandLineOptions.

--- CertPairGenerator/CommandLineOptions.cs
+++ CertPairGenerator/CommandLineOptions.cs
@@ -10,8 +10,6 @@ namespace CertPairGenerator
 {
     public class CommandLineOptions
     {
-        private string _DestinationFolder;
-
         [Option('d', "destinationFolder", Required = false, MetaValue = "DIRPATH",
                 HelpText = "Output directory to create certificates.")]
         public string DestinationFolder { get; set; }
