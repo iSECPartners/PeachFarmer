b41293bf5cb81cb70553009d442036c91ece78a9
Michael Lynch <mlynch@isecpartners.com>
2014-01-02 21:37:30 -0500

    Adding missing test classes.

---
+++ PeachFarmerTest/AssignWorkRequestProcessorTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoteHarvester;
+using RemoteHarvesterTest.MockObjects;
+using PeachFarmerLib.Messages;
+
+namespace RemoteHarvesterTest
+{
+    [TestClass]
+    public class AssignWorkRequestProcessorTest
+    {
+        private AssignWorkResponseMessage ProcessAssignWorkRequest(AssignWorkRequestProcessor processor, UInt64 startIteration, UInt64 endIteration, string password)
+        {
+            AssignWorkRequestMessage request = new AssignWorkRequestMessage(startIteration, endIteration, password);
+            return (AssignWorkResponseMessage)processor.Process(request);
+        }
+
+        [TestMethod]
+        public void ProcessTest()
+        {
+            UInt64 startIteration = 50;
+            UInt64 endIteration = 75;
+            string password = null;
+            MockLauncherService mockLauncher = new MockLauncherService();
+            AssignWorkRequestProcessor processor = new AssignWorkRequestProcessor(mockLauncher, password);
+            AssignWorkResponseMessage response = ProcessAssignWorkRequest(processor, startIteration, endIteration, password);
+
+            Assert.IsTrue(response.IsPasswordCorrect);
+            Assert.AreEqual(AssignWorkResponseMessage.AssignmentResult.Succeeded, response.Result);
+            Assert.AreEqual(startIteration, mockLauncher.AssignedStartIteration);
+            Assert.AreEqual(endIteration, mockLauncher.AssignedEndIteration);
+            Assert.IsFalse(mockLauncher.Killed);
+        }
+
+        [TestMethod]
+        public void ProcessWrongPasswordTest()
+        {
+            string correctPassword = "c0rr3ct_p4ssword!";
+            string suppliedPassword = "cheese";
+            MockLauncherService mockLauncher = new MockLauncherService();
+            AssignWorkRequestProcessor processor = new AssignWorkRequestProcessor(mockLauncher, correctPassword);
+            AssignWorkResponseMessage response = ProcessAssignWorkRequest(processor, 50, 75, suppliedPassword);
+
+            Assert.IsFalse(response.IsPasswordCorrect);
+            Assert.AreEqual(AssignWorkResponseMessage.AssignmentResult.Unknown, response.Result);
+            Assert.IsNull(mockLauncher.AssignedStartIteration);
+            Assert.IsNull(mockLauncher.AssignedEndIteration);
+            Assert.IsFalse(mockLauncher.Killed);
+        }
+    }
+}
---
+++ PeachFarmerTest/MockObjects/MockLauncherService.cs
@@ -0,0 +1,36 @@
+﻿using PeachLauncher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteHarvesterTest.MockObjects
+{
+    class MockLauncherService : ILauncherService
+    {
+        public UInt64? AssignedStartIteration { get; private set; }
+
+        public UInt64? AssignedEndIteration { get; private set; }
+
+        public bool Killed { get; private set; }
+
+        public MockLauncherService()
+        {
+            AssignedStartIteration = null;
+            AssignedEndIteration = null;
+            Killed = false;
+        }
+
+        public void Launch(UInt64 startIteration, UInt64 endIteration)
+        {
+            AssignedStartIteration = startIteration;
+            AssignedEndIteration = endIteration;
+        }
+
+        public void Kill()
+        {
+            Killed = true;
+        }
+    }
+}
